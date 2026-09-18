88b80d3c331a846fdeca50ef7e616d36ae88c6cd
saichandu0511 <saichandanreddy226@gmail.com>
2026-09-18 14:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-	let prices = document.querySelectorAll(".price"); 
-	let total=0;
-	for(let price of prices){
+	let prices = document.querySelectorAll(".price");
+
+	let total = 0;
+
+	for (let price of prices){
 		total = total + Number(price.innerText);
 	}
+
 	let row = document.createElement("tr");
 	let cell = document.createElement("td");
-	
 	cell.innerText = total;
 	row.appendChild(cell);
-	
 	document.querySelector("table").appendChild(row);
 };
 
